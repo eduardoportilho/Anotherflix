59b8846c0129e947b0047a6ee60ce44988852e81
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-03-29 17:30:33 +0200

    Factories and props

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -5,20 +5,22 @@ var MyTitle = React.createClass({
   render: function () {
     return (
       div(null,
-        h1(null, 'My Title')
+        h1({ style: {color: this.props.color} }, this.props.title)
       )
     )
   }
 })
 
+var MyTitleFactory = React.createFactory(MyTitle)
+
 var MyFirstComponent = React.createClass({  
   render: function () {
     return (
       div(null,
-        React.createElement(MyTitle),
-        React.createElement(MyTitle),
-        React.createElement(MyTitle),
-        React.createElement(MyTitle)
+        MyTitleFactory({title: 'Props are the best', color: 'peru'}),
+        MyTitleFactory({title: 'semicolons are the best', color: 'tomato'}),
+        MyTitleFactory({title: 'yada', color: 'darkgoldenrod'}),
+        MyTitleFactory({title: 'qqqq', color: 'papayawhip'})
       )
     )
   }
