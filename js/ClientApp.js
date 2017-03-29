48f4426173a5a8c9cf767a94702cb410907e0b4a
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-03-29 17:20:19 +0200

    Second Component

---
+++ js/ClientApp.js
@@ -0,0 +1,26 @@
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render: function () {
+    return (
+      div(null,
+        h1(null, 'My Title')
+      )
+    )
+  }
+})
+
+var MyFirstComponent = React.createClass({  
+  render: function () {
+    return (
+      div(null,
+        React.createElement(MyTitle),
+        React.createElement(MyTitle),
+        React.createElement(MyTitle),
+        React.createElement(MyTitle)
+      )
+    )
+  }
+})
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
