03aa4bab06962f42d75a75bb9886bae5f410cf78
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-05 16:51:41 +0200

    Init routing, extract Landing for its own component

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
-import '../public/normalize.css'
-import '../public/style.css'
+import { HashRouter, Match } from 'react-router'
+import Landing from './Landing'
 
 const App = React.createClass({
   render () {
     return (
-      <div className='app'>
-        <div className='landing'>
-          <h1>svideo</h1>
-          <input type='text' placeholder='Search' />
-          <a>or Browse All</a>
+      <HashRouter>
+        <div className='app'>
+          <Match exactly pattern='/' component={Landing} />
         </div>
-      </div>
+      </HashRouter>
     )
   }
 })
+
 render(<App />, document.getElementById('app'))
