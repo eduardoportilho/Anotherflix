55e31801016159b87c59f539963603ee383114a7
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-05-03 16:48:10 +0200

    Details skeleton

---
+++ js/Details.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Details = React.createClass({
+  render () {
+    return (
+      <div className='details'>
+        <h1>lolhi</h1>
+      </div>
+    )
+  }
+})
+
+export default Details
