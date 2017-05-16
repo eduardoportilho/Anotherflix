fc7bac2ff298082770c053b94b8397fcbfd6d6e6
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-05-16 18:03:21 +0200

    Move async rating fetch to redux

--- js/Details.js
+++ js/Details.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import axios from 'axios'
 import Header from './Header'
+import { connect } from 'react-redux'
+import { getOMDBDetails } from './actionCreators'
 
-const { shape, string } = React.PropTypes
+const { shape, string, func } = React.PropTypes
 
 const Details = React.createClass({
   propTypes: {
@@ -12,28 +13,22 @@ const Details = React.createClass({
       year: string,
       poster: string,
       trailer: string
-    })
-  },
-  getInitialState () {
-    return {
-      omdbData: {}
-    }
+    }),
+    omdbData: shape({
+      imdbID: string
+    }),
+    dispatch: func
   },
   componentDidMount () {
-    axios.get(`http://www.omdbapi.com/?i=${this.props.show.imdbID}`)
-      .then((response) => {
-        console.log('response', response)
-        this.setState({omdbData: response.data})
-      })
-      .catch((error) => {
-        console.error('axios error', error)
-      })
+    if (!this.props.omdbData.imdbRating) {
+      this.props.dispatch(getOMDBDetails(this.props.show.imdbID))
+    }
   },
   render () {
     const { title, description, year, poster, trailer } = this.props.show
     let rating
-    if (this.state.omdbData.imdbRating) {
-      rating = <h3>{this.state.omdbData.imdbRating}</h3>
+    if (this.props.omdbData.imdbRating) {
+      rating = <h3>{this.props.omdbData.imdbRating}</h3>
     } else {
       rating = <img src='/public/img/loading.png' alt='loading indicator' />
     }
@@ -55,4 +50,11 @@ const Details = React.createClass({
   }
 })
 
-export default Details
+const mapStateToProps = (state, ownProps) => {
+  const omdbData = state.omdbData[ownProps.show.imdbID] ? state.omdbData[ownProps.show.imdbID] : {}
+  return {
+    omdbData
+  }
+}
+
+export default connect(mapStateToProps)(Details)
