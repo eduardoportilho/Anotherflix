57674789a577ff84936b4a47c65db28a377fc99d
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-05-19 17:09:49 +0200

    Loading Landing with async route

--- js/App.js
+++ js/App.js
@@ -2,16 +2,24 @@ import React from 'react'
 import { Match } from 'react-router'
 import { Provider } from 'react-redux'
 import store from './store'
-import Landing from './Landing'
 import Search from './Search'
 import Details from './Details'
 import preload from '../public/data.json'
+import AsyncRoute from './AsyncRoute'
+
+if (global) {
+  global.System = { import () {} }
+}
 
 const App = () => {
   return (
     <Provider store={store}>
       <div className='app'>
-        <Match exactly pattern='/' component={Landing} />
+        <Match
+          exactly
+          pattern='/'
+          component={(props) => <AsyncRoute props={props} loadingPromise={System.import('./Landing')} />}
+        />
         <Match pattern='/search' component={(props) => <Search shows={preload.shows} {...props} />} />
         <Match pattern='/details/:id' component={(props) => {
           const show = preload.shows.filter((show) => props.params.id === show.imdbID)
